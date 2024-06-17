ff8b2c1d2716082715f9215795707ea7ab2a3e08
2mook2 <127025513+2mook2@users.noreply.github.com>
2024-06-17 16:18:15 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -14,8 +14,6 @@ const CONFIG = {
     {
       name: `Dementia_Analysis`,
       href: "https://github.com/yonghyeun/Dementia_Analysis",
-    },
-    {
       name: `HOD: Harmful Object Detection`,
       href: "https://github.com/poori-nuna/HOD-Benchmark-Dataset",
     },
